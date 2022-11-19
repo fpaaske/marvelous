f8cef9ae431f43ae86d096ccff7c94d1f63b4b24
Frank Paaske <frank@gintel.com>
2022-11-19 21:55:30 +0100

    feat: navigate 'home' when re-selecting current tab

--- src/features/tabs/tabs.component.ts
+++ src/features/tabs/tabs.component.ts
@@ -8,10 +8,11 @@ import { NavigationService } from "../../shared/services/navigation.service";
   templateUrl: "./tabs.component.html",
 })
 export class TabsComponent implements OnInit {
-  private outletMap: { [i: number]: string } = {
-    0: "charactersTab",
-    1: "seriesTab",
-  };
+  private outletMap: { [i: number]: { outlet: string; defaultRoute: string } } =
+    {
+      0: { outlet: "charactersTab", defaultRoute: "characters" },
+      1: { outlet: "seriesTab", defaultRoute: "series" },
+    };
   selectedTabIndex = 0;
 
   constructor(
@@ -35,16 +36,34 @@ export class TabsComponent implements OnInit {
       )
       .then(() => {
         this.navigationService.setCurrentOutlet(
-          this.outletMap[this.selectedTabIndex]
+          this.outletMap[this.selectedTabIndex].outlet
         );
         console.log("Navigated outlets");
       });
   }
 
   tabSelected(index: number) {
-    this.selectedTabIndex = index;
-    this.navigationService.setCurrentOutlet(
-      this.outletMap[this.selectedTabIndex]
-    );
+    const currentOutlet = this.outletMap[this.selectedTabIndex];
+    if (this.selectedTabIndex !== index) {
+      this.selectedTabIndex = index;
+      this.navigationService.setCurrentOutlet(currentOutlet.outlet);
+    } else {
+      const outlets = {};
+      outlets[currentOutlet.outlet] = [currentOutlet.defaultRoute];
+      console.log("Navigating home", outlets);
+      this.routerExtensions
+        .navigate(
+          [
+            {
+              outlets,
+            },
+          ],
+          {
+            relativeTo: this.activatedRoute,
+            clearHistory: true,
+          }
+        )
+        .then(() => console.log("Navigated home"));
+    }
   }
 }
